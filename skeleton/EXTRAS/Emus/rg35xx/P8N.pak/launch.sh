13b638bbefd5c31035dceddff95892db9a64f99d
Turro75 <valerio.turrini@gmail.com>
2024-08-27 21:39:58 +0200

    rg35xx: Fix native Pico8 audio too fast

--- skeleton/EXTRAS/Emus/rg35xx/P8N.pak/launch.sh
+++ skeleton/EXTRAS/Emus/rg35xx/P8N.pak/launch.sh
@@ -1,15 +1,9 @@
 #!/bin/sh
 
-# enable all CPU cores
-#echo 0xf > /sys/devices/system/cpu/autoplug/plug_mask
-#echo 1 > /sys/devices/system/cpu/cpu1/online
-#echo 1 > /sys/devices/system/cpu/cpu2/online
-#echo 1 > /sys/devices/system/cpu/cpu3/online
-
 progdir="${SDCARD_PATH}/Tools/${PLATFORM}/Pico-8 Splore.pak/pico8-native"
 thisdir=$(dirname "$0")
 cd "$progdir"
-echo "PICO-8 Starting Splore" > $thisdir/log.txt
+echo "PICO-8 Starting Splore" > "${thisdir}/log.txt"
 
 export SDL_VIDEODRIVER=directfb
 export SDL_AUDIODRIVER=alsa
@@ -20,13 +14,11 @@ overclock.elf $CPU_SPEED_GAME
 #overclock.elf $CPU_SPEED_PERF
 #overclock.elf $CPU_SPEED_MAX
 
+cp "${progdir}/patch/mypatch.conf" /usr/share/alsa/alsa.conf.d/mypatch.conf
+
 HOME="${progdir}" \
 ./pico8_dyn -v -run "${1}"  >> "${thisdir}/log.txt" 2>&1
 
-# restore default CPU cores state
-#echo 0x0 > /sys/devices/system/cpu/autoplug/plug_mask
-#echo 0 > /sys/devices/system/cpu/cpu1/online
-#echo 0 > /sys/devices/system/cpu/cpu2/online
-#echo 0 > /sys/devices/system/cpu/cpu3/online
-
+#restore previous state
+rm -rf /usr/share/alsa/alsa.conf.d/mypatch.conf
 overclock.elf $CPU_SPEED_MENU
--- skeleton/EXTRAS/Tools/rg35xx/Pico-8 Splore.pak/launch.sh
+++ skeleton/EXTRAS/Tools/rg35xx/Pico-8 Splore.pak/launch.sh
@@ -3,16 +3,10 @@
 #set here the dir where splore looks for carts
 romdir="${SDCARD_PATH}/Roms/Pico-8 (P8)"
 
-# enable all CPU cores
-echo 0xf > /sys/devices/system/cpu/autoplug/plug_mask
-echo 1 > /sys/devices/system/cpu/cpu1/online
-echo 1 > /sys/devices/system/cpu/cpu2/online
-echo 1 > /sys/devices/system/cpu/cpu3/online
-
 thisdir=$(dirname "$0")
 progdir="$thisdir/pico8-native"
 cd "$progdir"
-echo "PICO-8 Starting Splore" > $thisdir/log.txt
+echo "PICO-8 Starting Splore" > "${thisdir}/log.txt"
 
 export SDL_VIDEODRIVER=directfb
 export SDL_AUDIODRIVER=alsa
@@ -23,13 +17,11 @@ overclock.elf $CPU_SPEED_GAME
 #overclock.elf $CPU_SPEED_PERF
 #overclock.elf $CPU_SPEED_MAX
 
+cp "${progdir}/patch/mypatch.conf" /usr/share/alsa/alsa.conf.d/mypatch.conf
+
 HOME="${progdir}" \
-./pico8_dyn -v -splore -root_path "${romdir}" >> $thisdir/log.txt 2>&1
+./pico8_dyn -v -splore -root_path "${romdir}" >> "${thisdir}/log.txt" 2>&1
 
-# restore default CPU cores state
-echo 0x0 > /sys/devices/system/cpu/autoplug/plug_mask
-echo 0 > /sys/devices/system/cpu/cpu1/online
-echo 0 > /sys/devices/system/cpu/cpu2/online
-echo 0 > /sys/devices/system/cpu/cpu3/online
-
+#restore previous state
+rm -rf /usr/share/alsa/alsa.conf.d/mypatch.conf
 overclock.elf $CPU_SPEED_MENU
