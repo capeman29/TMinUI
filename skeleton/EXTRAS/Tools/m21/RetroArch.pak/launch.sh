4a77c7885a8212f4e71f591206e372dcc980b07e
Turro75 <valerio.turrini@gmail.com>
2024-11-26 23:40:08 +0100

    m21: remove retroarch from Tools

--- skeleton/EXTRAS/Tools/m21/RetroArch.pak/launch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-RA_HOME = ${BIOS_PATH}/RETROARCH
-cd ${RA_HOME}
-retroarch.elf --features -v &> "$LOGS_PATH/RetroArchFeatures.txt"
-retroarch.elf --help -v &> "$LOGS_PATH/RetroArchHelp.txt"
-retroarch.elf --config ${BIOS_PATH}/RETROARCH/${PLATFORM}/retroarch.cfg --menu -v &> "$LOGS_PATH/RetroArch.txt"
